ad8343cd3ff1eba510282fbd34b13e4e94c91a67
Zealll <eriznis@gmail.com>
2019-04-09 17:58:54 -0400

    Day 2 MVP done

--- users/middleware.js
+++ users/middleware.js
@@ -2,26 +2,50 @@
 const bcrypt = require('bcryptjs')
 const db = require('./users-helpers.js')
 
-
 function restricted(req, res, next) {
-    const { username, password } = req.headers
-
-    if (username && password) {
-        db 
-        .findBy({ username })
-        .first()
-        .then(user => {
-            if (user && bcrypt.compareSync(password, user.password)){
-                next()
-            } else {
-                res.status(401).json({message: "Invalid Username and/or Password"})
-            }
-        })
-        .catch(error => res.status(500).json(error))
-    } else {
-        res.json({message: "Forgot one of the credentials"})
+    if(req.session && req.session.user) {
+        next()
+    } else  {
+        res.status(401).json({message: "You shall not pass!"})
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// ======== OLD MIDDLEWARE =========
+
+
+// function restricted(req, res, next) {
+//     const { username, password } = req.headers
+
+//     if (username && password) {
+//         db 
+//         .findBy({ username })
+//         .first()
+//         .then(user => {
+//             if (user && bcrypt.compareSync(password, user.password)){
+//                 next()
+//             } else {
+//                 res.status(401).json({message: "Invalid Username and/or Password"})
+//             }
+//         })
+//         .catch(error => res.status(500).json(error))
+//     } else {
+//         res.json({message: "Forgot one of the credentials"})
+//     }
+// }
 
 
 module.exports = {
--- users/users-routers.js
+++ users/users-routers.js
@@ -9,7 +9,7 @@ const db = require('./users-helpers.js')
 
 
 
-
+// Endpoint for all the users if logged in
 router.get('/', restricted,(req, res) => {
     db
     .get()
@@ -17,6 +17,22 @@ router.get('/', restricted,(req, res) => {
     .catch(error => res.status(500).json(error))
 })
 
+//Endpoint for LOGOUT
+router.get('/logout', (req, res) => {
+    if(req.session) {
+        req.session.destroy(error => {
+            if(error) {
+                res.status(500).json({message: "You shall no Leaveeee!!!!"})
+            } else {
+                res.send('See you soon!')
+            }
+        })
+    } else {
+        res.status(401).json({message: "Please make sure your session is working."})
+    }
+})
+
+//endpoint for REGISTERING
 router.post('/register', (req,res) => {
     let user = req.body
     const hash = bcrypt.hashSync(user.password, 4)
@@ -28,6 +44,7 @@ router.post('/register', (req,res) => {
     .catch(error => res.status(500).json(error))
 })
 
+//endpoint for LOGIN
 router.post('/login',  (req,res) => {
     let { username, password } = req.body
 
@@ -35,6 +52,7 @@ router.post('/login',  (req,res) => {
     .findBy({ username })
     .first()
     .then(user => {
+        req.session.user = user
         if (user && bcrypt.compareSync(password, user.password)) {
             res.status(200).json({message: `Logged In! Your ID is ${user.id}`})
         } else {
